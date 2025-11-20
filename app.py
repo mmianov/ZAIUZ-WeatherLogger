6bd204650481f66532c2676ecc82d33f08a3ddca
mmianov <64694818+mmianov@users.noreply.github.com>
2025-11-20 02:17:55 +0100

    Update app.py

--- app.py
+++ app.py
@@ -48,4 +48,4 @@ swagger = Swagger(app, template=swagger_template)
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=False)
+    app.run(debug=False, host="0.0.0.0")
